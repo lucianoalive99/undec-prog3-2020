467dbccce9176c60c100d1d5d4f506ead23e579e
Luciano Alive <lucianoalive99@gmail.com>
2020-05-20 23:24:47 -0300

    test aeropuerto
    test solicitados de la clase aeropuerto

--- TP2/Aeropuerto/src/test/java/clasesTest/AeropuertoTest.java
+++ TP2/Aeropuerto/src/test/java/clasesTest/AeropuertoTest.java
@@ -1,12 +1,89 @@
 package clasesTest;
 
+import clases.Aerolinea;
 import clases.Aeropuerto;
+import exepciones.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AeropuertoTest {
 
+    @Test
+    void crearAeropuerto_creacionCorrecta() throws NuloException, StringNuloException, EnteroNegativoException, CodigoIATAException, NombreApellidoFormatoIncorrectoException {
 
+        Aeropuerto elAeropuerto = new Aeropuerto("EZE","Ministro Pistarini","Ezeiza",5360);
 
+        assertEquals("EZE", elAeropuerto.getIATA());
+        assertEquals("Ministro Pistarini", elAeropuerto.getNombre());
+        assertEquals("Ezeiza", elAeropuerto.getCiudad());
+        assertEquals(5360, elAeropuerto.getCodigoPostal());
+    }
+
+    @Test
+    void crearAeropuerto_creacionIncorrectaIataNulo()  {
+        assertThrows(NuloException.class,
+                () -> new Aeropuerto(null, "Ministro Pistarini","Ezeiza",5360));
+    }
+
+    @Test
+    void crearAeropuerto_creacionIncorrectaIataVacio()  {
+        assertThrows(StringNuloException.class,
+                () -> new Aeropuerto("", "Ministro Pistarini","Ezeiza",5360));
+    }
+
+    @Test
+    void crearAeropuerto_creacionIncorrectaNombreNulo()  {
+        assertThrows(NuloException.class,
+                () -> new Aeropuerto("EZE", null,"Ezeiza",5360));
+    }
+
+    @Test
+    void crearAeropuerto_creacionIncorrectaNombreVacio()  {
+        assertThrows(NuloException.class,
+                () -> new Aeropuerto("EZE", "","Ezeiza",5360));
+    }
+
+    @Test
+    void crearAeropuerto_creacionIncorrectaCiudadNula()  {
+        assertThrows(NuloException.class,
+                () -> new Aeropuerto("EZE", "Ministro Pistarini",null,5360));
+    }
+
+    @Test
+    void crearAeropuerto_creacionIncorrectaCiudadVacia()  {
+        assertThrows(NuloException.class,
+                () -> new Aeropuerto("EZE", "Ministro Pistarini","",5360));
+    }
+
+    @Test
+    void crearAeropuerto_creacionIncorrectaPostalNulo()  {
+        assertThrows(EnteroNegativoException.class,
+                () -> new Aeropuerto("EZE", "Ministro Pistarini","Ezeiza",null));
+    }
+
+    @Test
+    void crearAeropuerto_creacionIncorrectaPostalVacio()  {
+        assertThrows(EnteroNegativoException.class,
+                () -> new Aeropuerto("EZE", "Ministro Pistarini","Ezeiza",0));
+    }
+
+    @Test
+    void crearAeropuerto_creacionIncorrectaIataFormatoIncumplido()  {
+        assertThrows(CodigoIATAException.class,
+                () -> new Aeropuerto("EZ", "Ministro Pistarini","Ezeiza",5360));
+    }
+
+    @Test
+    void crearAeropuerto_creacionIncorrectaNombreFormatoIncumplido()  {
+        assertThrows(NombreApellidoFormatoIncorrectoException.class,
+                () -> new Aeropuerto("EZE", "ministro Pistarini","Ezeiza",5360));
+    }
+
+    @Test
+    void crearAeropuerto_creacionIncorrectaCiudadFormatoIncumplido()  {
+        assertThrows(NombreApellidoFormatoIncorrectoException.class,
+                () -> new Aeropuerto("EZE", "Ministro Pistarini","ezeiza",5360));
+    }
 }
